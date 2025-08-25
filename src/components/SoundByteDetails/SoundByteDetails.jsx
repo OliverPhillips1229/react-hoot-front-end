0a6bfaaacbdfdcf30932ed8decb65bb40ebfa616
alicia.brandewie@gmail.com <alicia@Alicia-PC.localdomain>
2025-08-25 13:18:52 -0500

    Components > SoundByteDetails updated

--- src/components/SoundByteDetails/SoundByteDetails.jsx
+++ src/components/SoundByteDetails/SoundByteDetails.jsx
@@ -2,71 +2,71 @@ import AuthorInfo from '../AuthorInfo/AuthorInfo';
 import { useParams, Link } from 'react-router-dom';
 import Icon from '../Icon/Icon';
 import { useState, useEffect, useContext } from 'react';
-import * as hootService from '../../services/hootService';
+import * as soundByteService from '../../services/soundByteService'; //confirm names align
 import CommentForm from '../CommentForm/CommentForm';
-import styles from './HootDetails.module.css';
+import styles from './SoundByteDetails.module.css';//confirm names align
 import Loading from '../Loading/Loading';
 import { UserContext } from '../../contexts/UserContext';
 
 
-const HootDetails = ({ handleDeleteHoot }) => {
+const SoundByteDetails = ({ handleDeleteSoundByte }) => {
   const handleDeleteComment = async (commentId) => {
     console.log('commentId:', commentId);
-    await hootService.deleteComment(hootId, commentId);
-    setHoot({
-      ...hoot,
-      comments: hoot.comments.filter((comment) => comment._id !== commentId),
+    await soundByteService.deleteComment(soundByteId, commentId);
+    setSoundByte({
+      ...soundByte,
+      comments: soundByte.comments.filter((comment) => comment._id !== commentId),
     });
   };
   const handleAddComment = async (commentFormData) => {
-    const newComment = await hootService.createComment(hootId, commentFormData);
-    setHoot({ ...hoot, comments: [...hoot.comments, newComment] });
+    const newComment = await soundByteService.createComment(soundByteId, commentFormData);
+    setSoundByte({ ...soundByte, comments: [...soundByte.comments, newComment] });
   };
-  const { hootId } = useParams();
-  const [hoot, setHoot] = useState(null);
+  const { soundByteId } = useParams();
+  const [soundByte, setSoundByte] = useState(null);
   const { user } = useContext(UserContext);
   useEffect(() => {
-    const fetchHoot = async () => {
-      const hootData = await hootService.show(hootId);
-      setHoot(hootData);
+    const fetchSoundByte = async () => { //confirm names align
+      const soundByteData = await soundByteService.show(soundByteId);
+      setSoundByte(soundByteData);
     };
-    fetchHoot();
-  }, [hootId]);
+    fetchSoundByte();
+  }, [soundByteId]);
 
-  console.log('hoot state:', hoot);
+  console.log('soundByte state:', soundByte);
 
-  if (!hoot) return <Loading />;
+  if (!soundByte) return <Loading />;
 
   return (
     <main className={styles.container}>
       <section>
         <header>
-          <p>{hoot.category.toUpperCase()}</p>
-          <h1>{hoot.title}</h1>
+          <p>{soundByte.category.toUpperCase()}</p>
+          <h1>{soundByte.title}</h1>
           <div>
             {/* Removed duplicate author/date <p> tag, now only using AuthorInfo */}
-      <AuthorInfo content={hoot} />
-            {hoot.author._id === user._id && (
+      <AuthorInfo content={soundByte} />
+            {soundByte.author._id === user._id && (
               <>
-                <Link to={`/hoots/${hootId}/edit`}>
+                <Link to={`/soundBytes/${soundByteId}/edit`}>
                   <Icon category='Edit' />
                 </Link>
-                <button onClick={() => handleDeleteHoot(hootId)}>
+                <button onClick={() => handleDeleteSoundByte(soundByteId)}>
                   <Icon category='Trash' />
                 </button>
               </>
             )}
           </div>
         </header>
-        <p>{hoot.text}</p>
+        <p>{soundByte.text}</p>
       </section>
       {/* All updates are in the comments section! */}
       <section>
         <h2>Comments</h2>
         {/* Pass the handleAddComment function to the CommentForm Component */}
         <CommentForm handleAddComment={handleAddComment}/>
-        {!hoot.comments?.length && <p>There are no comments.</p>}
-        {(hoot.comments || []).map((comment) => (
+        {!soundByte.comments?.length && <p>There are no comments.</p>}
+        {(soundByte.comments || []).map((comment) => (
           <article key={comment._id}>
             <header>
               <div>
@@ -74,7 +74,7 @@ const HootDetails = ({ handleDeleteHoot }) => {
                   <AuthorInfo content={comment} />
                 {comment.author._id === user._id && (
                   <>
-                    <Link to={`/hoots/${hootId}/comments/${comment._id}/edit`}>
+                    <Link to={`/soundBytes/${soundByteId}/comments/${comment._id}/edit`}>
                       <Icon category='Edit' />
                     </Link>
                     <button onClick={() => handleDeleteComment(comment._id)}>
@@ -92,4 +92,4 @@ const HootDetails = ({ handleDeleteHoot }) => {
   );
 };
 
-export default HootDetails;
+export default SoundByteDetails;
