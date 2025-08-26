fcf16ef08219e2836a9e8d4a421a0928d528eabe
Oliver Levine <oliversfphillips@icloud.com>
2025-08-26 09:23:27 -0700

    (LOADING.JSX) Fixed a small bug, it now renders in the browser

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,10 +1,8 @@
 import styles from './Loading.module.css';
-import LoadingIcon from '../../assets/images/loading.svg';
 
 const Loading = () => {
   return (
     <main className={styles.container}>
-      <img src={LoadingIcon} alt='A cute owl' />
     </main>
   );
 };
