ed327e19e3cacc94cd02ce2eb1d263a9f794c60b
Bastian Pethel <bastianpethel360@gmail.com>
2025-08-27 09:55:53 -0400

    removed dropdown from nav bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,20 +1,13 @@
 import { Link, useLocation } from 'react-router-dom';
-import { useState } from 'react';
 import styles from './NavBar.module.css';
 
 const NavBar = ({ user, handleSignOut }) => {
-  const [isProfileDropdownOpen, setIsProfileDropdownOpen] = useState(false);
   const location = useLocation();
 
   const onSignOut = () => {
     localStorage.removeItem('token');
     localStorage.removeItem('rememberUser');
     handleSignOut && handleSignOut();
-    setIsProfileDropdownOpen(false);
-  };
-
-  const toggleProfileDropdown = () => {
-    setIsProfileDropdownOpen(!isProfileDropdownOpen);
   };
 
   const isActive = (path) => {
@@ -54,51 +47,16 @@ const NavBar = ({ user, handleSignOut }) => {
             </ul>
 
             <div className={styles.userSection}>
-              <div className={styles.profileDropdown}>
-                <button
-                  className={styles.profileButton}
-                  onClick={toggleProfileDropdown}
-                  aria-label="User menu"
-                >
+              <div className={styles.profile}>
+                <Link to={`/profile/${user.username}`}>
                   <div className={styles.avatar}>
                     {user.displayName ? user.displayName.charAt(0).toUpperCase() : 'U'}
                   </div>
                   <span className={styles.username}>{user.displayName || user.username}</span>
-                  <span className={styles.dropdownArrow}>▼</span>
+                </Link>
+                <button onClick={onSignOut} className={styles.signOutBtn}>
+                  Sign Out
                 </button>
-
-                {isProfileDropdownOpen && (
-                  <div className={styles.dropdownMenu}>
-                    <Link
-                      to={`/profile/${user.username}`}
-                      onClick={() => setIsProfileDropdownOpen(false)}
-                    >
-                      My Profile
-                    </Link>
-                    <Link
-                      to='/settings'
-                      onClick={() => setIsProfileDropdownOpen(false)}
-                    >
-                      Settings
-                    </Link>
-                    <Link
-                      to='/my-sounds'
-                      onClick={() => setIsProfileDropdownOpen(false)}
-                    >
-                      My Sounds
-                    </Link>
-                    <Link
-                      to='/following'
-                      onClick={() => setIsProfileDropdownOpen(false)}
-                    >
-                      Following
-                    </Link>
-                    <hr />
-                    <button onClick={onSignOut} className={styles.signOutBtn}>
-                      Sign Out
-                    </button>
-                  </div>
-                )}
               </div>
             </div>
           </>
